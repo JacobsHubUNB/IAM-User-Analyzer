c5c1d0c31df6739efebd600ad0d3257b0e294cc9
n69yp@unb.ca <n69yp@unb.ca>
2026-03-03 20:07:08 -0400

    Foundational addtions made to the AWS script IAM.py

--- Scripts/IAM.py
+++ Scripts/IAM.py
@@ -3,12 +3,35 @@ import json
 import networkx as nx
 from urllib.parse import unquote 
 
+#Script accessing AWS account
 iam = boto3.client('iam') 
 
 graph = nx.DiGraph()
 
+#list containing the security threats of each resource for every policy for every role
 
+security_threats = []
 
+#=====================================================================
+# DATA EXTRACTION AND PROCESSING
+#=====================================================================
 
-print(f'{dir(iam)}')
+def analyze(policy_doc, policies):
 
+    graph.add_node(policy_name)
+
+roles = iam.list_roles()['Roles']
+
+#analyze every policy for every role
+
+for role in roles:
+    role_name = role['Rolename']
+    policies = iam.list_attached_role_policies(RoleName=role_name)
+    policydict = role['AssumeRolePolicyDocument']
+    policy_names = role['AttachedPolicies']
+    graph.add_node(role_name)
+    analyze(policydict,policies )
+
+#=====================================================================
+# FINDING PRINITOUT
+#=====================================================================
