e93f167c8609d9d454db350a5b11a1538f55fe0a
n69yp@unb.ca <n69yp@unb.ca>
2026-03-04 21:36:46 -0400

    Further development Made to Analyze method in the IAM.py script. Implemented a nested dictionary for the security report. Added both .txt and .json files for structural analysis of typical IAM user identities.

--- Scripts/IAM.py
+++ Scripts/IAM.py
@@ -8,30 +8,52 @@ iam = boto3.client('iam')
 
 graph = nx.DiGraph()
 
-#list containing the security threats of each resource for every policy for every role
+#list containing the security threats of every policy of each role
 
-security_threats = []
+Role_Security_Threats = {}
 
 #=====================================================================
 # DATA EXTRACTION AND PROCESSING
 #=====================================================================
 
-def analyze(policy_doc, policies):
+def analyze( role, statement, rlst):
+    
+    effect = statement.get('Effect', 'Allow')
 
+    action = statement.get('Action', [])
+
+    if isinstance(action, str):   # if it's just one string
+            actions = [action]
+
+    resource = statement.get('Resource', [])
+
+    if isinstance(resource, str):   # if it's just one string
+            resources = [resource]
+
+    exempt = False
+    for act in actions:
+        if 
+        if '*' in act
     graph.add_node(policy_name)
+    
 
 roles = iam.list_roles()['Roles']
 
 #analyze every policy for every role
 
 for role in roles:
-    role_name = role['Rolename']
     policies = iam.list_attached_role_policies(RoleName=role_name)
-    policydict = role['AssumeRolePolicyDocument']
-    policy_names = role['AttachedPolicies']
-    graph.add_node(role_name)
-    analyze(policydict,policies )
+    graph.add_node( role['RoleName'])
+    Role_Security_Threats['Role_Analysis'] = {'RoleName': role['RoleName'], 'Policies': [] }
+    for policy in policies:
+        policy_name = ploicy['PolicyName']
+        policyReport = {'Policy_Name': policy_name, 'Statement_Report': []}
+        Role_Security_Threats['Role_Analysis']['Policies'].append(policyStmt)
+        graph.add_node(policy_name)
+        policy_statement = policy['PolicyDocument']['Statement']
+        analyze(role['RoleName'],policy_statement, policyReport)
+
 
 #=====================================================================
-# FINDING PRINITOUT
+# FINDINGS PRINITOUT
 #=====================================================================
